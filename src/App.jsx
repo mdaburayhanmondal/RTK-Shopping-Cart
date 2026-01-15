6690b2092aad33c7f6b7de4cde7c5ecb496aa9b5
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-15 07:56:56 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import './App.css';
+import { Outlet } from 'react-router';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <h1 className="text-5xl">Hello</h1>
-    </>
+    <div className="min-h-screen bg-[#FDFDFD] text-gray-900">
+      <Navbar />
+      <main className="pb-20">
+        <Outlet />
+      </main>
+      <footer className="py-12 border-t border-gray-100 bg-white">
+        <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-6">
+          <p className="text-gray-400 font-medium">
+            © 2026 Minimal Store Practice Project
+          </p>
+          <div className="flex gap-x-8 text-sm text-gray-500 font-semibold">
+            <a href="#" className="hover:text-blue-600">
+              Privacy Policy
+            </a>
+            <a href="#" className="hover:text-blue-600">
+              Terms of Service
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/home/Home.jsx';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+import Cart from './pages/Cart.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
